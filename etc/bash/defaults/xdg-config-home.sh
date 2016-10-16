486a2da39f092e7da369398b38887883f49a9d1a
Mark Norman Francis <norm@vanster.lan>
2016-10-16 22:25:39 +0100

    Set XDG config files to live in ~/etc

---
+++ etc/bash/defaults/xdg-config-home.sh
@@ -0,0 +1,6 @@
+# github:etc/bash/defaults/xdg-config-home.sh
+#
+# Set configuration files to live in ~/etc for software that supports
+# XDG_CONFIG_HOME (eg. git).
+
+export XDG_CONFIG_HOME="$HOME/etc"
